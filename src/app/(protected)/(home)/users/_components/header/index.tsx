8d9464749369860cd6fe513317d10e18bb86dfa0
Issam El jaouhary <iseljao89@gmail.com>
2024-08-07 12:45:25 -0400

    fix: update users page header style

--- src/app/(protected)/(home)/users/_components/header/index.tsx
+++ src/app/(protected)/(home)/users/_components/header/index.tsx
@@ -3,10 +3,12 @@ import ParamsSearch from "@/components/params-search";
 
 export default async function Page({ total }: { total: number }) {
   return (
-    <header className="items-between flex justify-between gap-4">
-      <h3>{`Users (${total})`}</h3>
+    <header className="items-between flex flex-wrap gap-4">
+      <h3 className="flex-1">{`Users (${total})`}</h3>
       <ParamsSearch />
-      <ParamsPagination total={total} />
+      <div className="flex flex-1 justify-end">
+        <ParamsPagination total={total} />
+      </div>
     </header>
   );
 }
